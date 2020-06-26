aeaace38e43dfe3c2ef427d1117308cc0f5aa7e7
Fambegbe Olamileke <olamileke.f@gmail.com>
2020-06-26 22:05:34 +0100

    refactoring

--- src/app/interceptors/loader.interceptor.ts
+++ src/app/interceptors/loader.interceptor.ts
@@ -11,7 +11,11 @@ export class LoaderInterceptor implements HttpInterceptor {
     constructor(private loader:LoaderService) {}
 
     intercept(req:HttpRequest<any>, next:HttpHandler): Observable<HttpEvent<any>> {
-        this.loader.show();
+
+        if(!req.url.includes('redirects')) {
+            this.loader.show();
+        }
+        
         return next.handle(req).pipe(finalize(() => this.loader.hide()));        
     }
 }
