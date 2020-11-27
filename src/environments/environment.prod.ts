9164ce04395d3cbde237914b1b0d16c39f432721
Fambegbe Olamileke <olamileke.f@gmail.com>
2020-11-27 20:04:47 +0100

    changed backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_url:'http://localhost:5000/api/',
+  api_url:'https://trimbackend.herokuapp.com/api/',
   client_url:'http://localhost:4200/',
   per_page:6
 };
