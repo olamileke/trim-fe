9b366a0b1ae902c81801857e9267d0f2cca977bf
Fambegbe Olamileke <olamileke.f@gmail.com>
2020-11-30 20:57:57 +0100

    pointing to correct app url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api_url:'https://trimbackend.herokuapp.com/api/',
-  client_url:'https://trimapp.netlify.app/',
+  client_url:'https://trimm.xyz/',
   per_page:6
 };
