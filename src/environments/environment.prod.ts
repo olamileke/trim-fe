c49de93ad6aa75aaf28458e6175db7be5533e2ea
Fambegbe Olamileke <olamileke.f@gmail.com>
2022-07-03 22:32:58 +0100

    chore: updated API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_url: 'https://trim.api.themilestonesapp.xyz/api/',
+  api_url: 'https://trim-api.themilestonesapp.xyz/api/',
   client_url: 'https://trimm.xyz/',
   per_page: 6,
 };
